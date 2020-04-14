e8a763eea0a8bd1be59190374aefd60b214213fc
zhouchao1992 <1669028439@qq.com>
2020-04-14 17:10:18 +0800

    公共模块中加入了自定义全局异常处理依赖

--- zc-security/src/main/java/com/zc/security/controller/TestController.java
+++ zc-security/src/main/java/com/zc/security/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.zc.security.controller;
 
+import com.purgeteam.dispose.starter.annotation.EnableGlobalDispose;
 import com.zc.common.result.Result;
 import com.zc.common.result.ResultUtil;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/test/")
+@EnableGlobalDispose//自定义异常捕获注解
 public class TestController {
 
     @GetMapping("result")
@@ -20,6 +22,17 @@ public class TestController {
         map.put("2","王五");
         map.put("3","李四");
         map.put("4","赵四");
+        return ResultUtil.success(map);
+    }
+
+    @GetMapping("exce")
+    public Result testexce(){
+        Map<String,Object> map = new HashMap<>();
+        map.put("1","张三");
+        map.put("2","王五");
+        map.put("3","李四");
+        map.put("4","赵四");
+        int a=10/0;
         return ResultUtil.success(map);
     }
 }
